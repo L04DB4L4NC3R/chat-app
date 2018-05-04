7efa9f6ab06db67f278b48a7030ad786d65fa3a2
angad <angadsharma1016@gmail.com>
2018-05-04 09:25:07 +0530

    Materialize front end

--- app.js
+++ app.js
@@ -13,5 +13,9 @@ app.use(bp.urlencoded({extended:false}));
 //static files middleware
 app.use(express.static('public'));
 
+app.get("/",(req,res)=>{
+    res.render('index');
+});
 
+console.log("listening on port 3000");
 const server = app.listen(process.env.PORT || 3000 );
